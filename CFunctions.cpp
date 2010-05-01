40ee1501b9ea6224d9dee48ed44db75858b46348
mabako <mabako@gmail.com>
2010-05-02 00:03:30 +0200

    sha1, sha224, sha256, sha384 and sha512

---
+++ CFunctions.cpp
@@ -0,0 +1,58 @@
+/*********************************************************
+*
+*  Multi Theft Auto: San Andreas - Deathmatch
+*
+*  ml_base, External lua add-on module
+*
+*  Copyright © 2003-2008 MTA.  All Rights Reserved.
+*
+*  Grand Theft Auto is © 2002-2003 Rockstar North
+*
+*  THE FOLLOWING SOURCES ARE PART OF THE MULTI THEFT
+*  AUTO SOFTWARE DEVELOPMENT KIT AND ARE RELEASED AS
+*  OPEN SOURCE FILES. THESE FILES MAY BE USED AS LONG
+*  AS THE DEVELOPER AGREES TO THE LICENSE THAT IS
+*  PROVIDED WITH THIS PACKAGE.
+*
+*********************************************************/
+
+#include "CFunctions.h"
+#include "extra/CLuaArguments.h"
+#include "sha/sha1.h"
+#include "sha/sha2.h"
+
+#define fn(a,b,c) \
+int CFunctions::b( lua_State* luaVM ) \
+{ \
+    if( luaVM ) \
+    { \
+        CLuaArguments args; \
+		if( lua_type ( luaVM, 1 ) == LUA_TSTRING || lua_type ( luaVM, 1 ) == LUA_TNUMBER ) \
+		{ \
+			unsigned char hval[ c ]; \
+			const char* data = lua_tostring( luaVM, 1 ); \
+			a( hval, (unsigned const char*) lua_tostring( luaVM, 1 ), lua_objlen( luaVM, 1 ) ); \
+			\
+			char pBuffer[ c*2+1 ]; \
+			for( unsigned int i = 0; i < c; ++ i ) \
+				sprintf ( pBuffer+i*2, "%02x", hval[i] ); \
+			args.PushString( pBuffer ); \
+		} \
+		else \
+		{ \
+			luaL_error( luaVM, "Bad argument @ '" #a "'" ); \
+			args.PushBoolean( false ); \
+		} \
+        args.PushArguments( luaVM ); \
+        return 1; \
+    } \
+    return 0; \
+}
+
+fn(sha1,f_sha1,SHA1_DIGEST_SIZE);
+fn(sha224,f_sha224,SHA224_DIGEST_SIZE);
+fn(sha256,f_sha256,SHA256_DIGEST_SIZE);
+fn(sha384,f_sha384,SHA384_DIGEST_SIZE);
+fn(sha512,f_sha512,SHA512_DIGEST_SIZE);
+
+#undef fn
---
+++ module.cpp
@@ -0,0 +1,61 @@
+/*********************************************************
+*
+*  Multi Theft Auto: San Andreas - Deathmatch
+*
+*  ml_base, External lua add-on module
+*  
+*  Copyright © 2003-2008 MTA.  All Rights Reserved.
+*
+*  Grand Theft Auto is © 2002-2003 Rockstar North
+*
+*  THE FOLLOWING SOURCES ARE PART OF THE MULTI THEFT
+*  AUTO SOFTWARE DEVELOPMENT KIT AND ARE RELEASED AS
+*  OPEN SOURCE FILES. THESE FILES MAY BE USED AS LONG
+*  AS THE DEVELOPER AGREES TO THE LICENSE THAT IS
+*  PROVIDED WITH THIS PACKAGE.
+*
+*********************************************************/
+
+#include "module.h"
+
+ILuaModuleManager10 *pModuleManager = NULL;
+
+// Initialisation function (module entrypoint)
+MTAEXPORT bool InitModule ( ILuaModuleManager10 *pManager, char *szModuleName, char *szAuthor, float *fVersion )
+{
+    pModuleManager = pManager;
+
+    // Set the module info
+    strncpy ( szModuleName, MODULE_NAME, MAX_INFO_LENGTH );
+    strncpy ( szAuthor, MODULE_AUTHOR, MAX_INFO_LENGTH );
+    (*fVersion) = MODULE_VERSION;
+
+    return true;
+}
+
+
+MTAEXPORT void RegisterFunctions ( lua_State * luaVM )
+{
+    if ( pModuleManager && luaVM )
+    {
+#define fn(a,b) pModuleManager->RegisterFunction ( luaVM, #a, CFunctions::b )
+fn(sha1,f_sha1);
+fn(sha224,f_sha224);
+fn(sha256,f_sha256);
+fn(sha384,f_sha384);
+fn(sha512,f_sha512);
+#undef fn
+    }
+}
+
+
+MTAEXPORT bool DoPulse ( void )
+{
+    return true;
+}
+
+MTAEXPORT bool ShutdownModule ( void )
+{
+    
+    return true;
+}
